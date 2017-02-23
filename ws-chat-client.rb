c555fed7bd6ce7311a5e9653e55de5764a5172f0
Daniel Seehausen <daniel.seehausen@gmail.com>
2017-02-22 23:12:57 -0500

    few things getting late just commit stop bothering me git

--- ws-chat-client.rb
+++ ws-chat-client.rb
@@ -1,7 +1,7 @@
 require 'websocket-client-simple'
 require 'io/console'
 
-url = ARGV.shift || 'ws://192.168.5.68:8080'
+url = ARGV.shift || 'ws://192.168.1.8:8080'
 ws = WebSocket::Client::Simple.connect url
 
 ws.on :message do |msg|
@@ -13,16 +13,20 @@ ws.on :open do
 end
 
 ws.on :close do |e|
+  puts "*** CLOSING ***"
   p e
   exit 1
 end
 
 ws.on :error do |e|
+  #TODO add attempt reconnect if error is server disconnected
   p e
   puts "Something went wrong (perhaps the server is no longer running?). Exiting..."
   exit -1
 end
 
 loop do
+  #to see bits of what you are typing swap comment on following two lines
+  # ws.send gets
   ws.send STDIN.noecho(&:gets)
 end
